0eca1c8b8cb56a67348db1098a0b68bba914865c
YoshieSuzuki <futurekame@gmail.com>
2021-04-26 11:05:10 +0900

    new

---
+++ Player.py
@@ -0,0 +1,59 @@
+import random
+
+class Player: #継承元となるプレイヤークラス。
+    def __init__(self,name,number):
+        self.name=name #名前
+        self.point=0 #得点
+        self.number=number #プレイヤーの番号。points[self.number]でも自分の得点を知れる。
+        self.action="stay" #行動(stay / theater / restaurant)。str型です注意。
+
+    def decide_action(self,points,actions,day): #継承前は何もしない。
+        pass
+
+#下のFujitaniクラスに倣って、Playerクラスを継承してください
+#与えられる情報としては、
+# 1,points(全てのプレイヤーの得点履歴が分かる)
+# 2,actions(前日の選択の分布が分かる)
+# 3,day(今日がシミュレーション何日目か)
+#与えられる情報については、printしてみると分かります
+
+#消してもらって大丈夫です。
+class Fujitani(Player): #例1
+    def __init__(self, name, number): #初期化。ここに追記する形でいじってもよい。
+        super().__init__(name, number)
+
+    def decide_action(self,points,actions,day): #ランダムに行動する人の例
+        tmp=["stay","theater","restaurant"]
+        self.action=random.choice(tmp)
+
+#消してもらって大丈夫です。
+class Tanaka(Player): #例2
+    def __init__(self, name, number):
+        super().__init__(name, number)
+
+    def decide_action(self, points, actions, day): #昨日の行動人数によって行動を決める人
+        if actions[2]<5: self.action="restaurant"
+        elif actions[1]<3: self.action="theater"
+        else: self.action="stay"
+
+class Suzuki(Player): #例1
+    def __init__(self, name: str, number: int): #初期化。ここに追記する形でいじってもよい。
+        super().__init__(name, number)
+
+    def decide_action(self,points,actions,day): #ランダムに行動する人の例
+        tmp=["stay","theater","restaurant"]
+        self.action=random.choice(tmp)
+
+#テンプレート
+#
+#class your_name(Player): #クラス名は苗字でお願いします。
+#    def __init__(self, name, number):
+#        super().__init__(name, number) これは消さないでください。
+#        self.temp=**
+#        -> self.***でインスタンス変数を定義できます。"self."を頭につけることで下のdecide_action関数内でも使えます。
+#
+#    def decide_action(self, points, actions, day):
+#        if ****: self.action=****
+#        elif ****: self.action=***
+#    　　という感じで、self.actionをルールベース/学習/etc...で決めてください。
+#
---
+++ game.py
@@ -0,0 +1,41 @@
+import Player
+
+def reward(stay,theater,restaurant): #行動別人数の組み合わせによって報酬を返す
+    reward_vec=[0,0,0]
+    if stay==10: reward_vec[0]=10
+    else: reward_vec[0]=1
+    if theater<=4: reward_vec[1]=20
+    elif theater<=7: reward_vec[1]=-10
+    else: reward_vec[1]=-15
+    if restaurant<=6: reward_vec[2]=40
+    elif restaurant<=8: reward_vec[2]=-25
+    else: reward_vec[2]=-35
+    return reward_vec
+
+trans={"stay":0,"theater":1,"restaurant":2}
+# TODO revert changes
+Players=[Player.Fujitani("yukitani",i) for i in range(8)] #今は全員藤谷でやってます
+Players.append(Player.Tanaka("Tanaka",8))
+Players.append(Player.Suzuki("Suzuki",9))
+#本番は,Players=[Player.Oguni("Oguni",0), Player.Suzuki("Suzuki",1), ... , Player.Matsumoto("Matsumoto",9)]こんな感じになります。
+hist=[[0] for _ in range(10)] #10人の点数推移メモ
+actions=[0,0,0] #stay,theater,restaurant
+points=[0 for _ in range(10)]
+
+for day in range(100): #3年のシミュレーション
+    # 行動決め
+    for player in Players: player.decide_action(points,actions,day) #ここで行動を決めます。
+
+    # 行動の集計
+    actions=[0,0,0] #stay,theater,restaurant
+    for player in Players: actions[trans[player.action]]+=1
+
+    # 報酬の計算と記録
+    reward_set=reward(actions[0], actions[1], actions[2]) #stay,theater,restaurantに対する報酬
+    for idx,player in enumerate(Players):
+        hist[idx].append(hist[idx][-1]+reward_set[trans[player.action]])
+        player.point=hist[idx][-1]
+        points[idx]=hist[idx][-1]
+    print(day)
+    print(actions)
+    print(points)
